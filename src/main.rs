6a099739904acda57d47318b7fd7f5f8d2dad65e
Finn Rudolph <finn.rudolph@t-online.de>
2023-11-30 18:38:32 +0100

    Implement basic Pollard's Rho

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+mod rho;
+
+use std::env;
+
+use rug::{Complete, Integer};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let n = Integer::parse(&args[1])
+        .expect("failed to parse the number to be factored")
+        .complete();
+
+    println!("=> {}", rho::pollard_rho(&n, 2));
+}
