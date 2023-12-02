d3a5eb515afffa82cead715f0a1e3f498118e97f
Finn Rudolph <finn.rudolph@t-online.de>
2023-12-02 11:38:13 +0100

    Evaluate formula for M = 2, F = 1

--- expected_iter.cpp
+++ expected_iter.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr size_t N = 1 << 12, F = 2;
+constexpr size_t N = 1 << 6, F = 1, M = 2;
 
 namespace nat
 {
@@ -44,23 +44,49 @@ namespace nat
     double gcd_prob(uint32_t k, uint32_t d) { return (double)phi[k / d] / k; }
 }
 
-double expected(uint32_t k, size_t i = 0, uint32_t d_sum = 0)
+double expected_m1(uint32_t k, size_t factor = 0, uint32_t d_sum = 0)
 {
-    if (i == F)
+    if (factor == F)
         return 1.0 / sqrt(d_sum);
 
     double e = 0.0;
     for (auto d : nat::div[k])
-        e += expected(k, i + 1, d_sum + d) * nat::gcd_prob(k, d);
+        e += expected_m1(k, factor + 1, d_sum + d) * nat::gcd_prob(k, d);
+    return e;
+}
+
+uint32_t _k[M];
+double inv_lg_k[M];
+
+double expected_f1(size_t machine = 0, double d_div_lg_sum = 0.0)
+{
+    if (machine == M)
+        return 1.0 / sqrt(d_div_lg_sum);
+
+    double e = 0.0;
+    for (auto d : nat::div[_k[machine]])
+        e += expected_f1(machine + 1, d_div_lg_sum +
+                                          (double)d * inv_lg_k[machine]) *
+             nat::gcd_prob(_k[machine], d);
     return e;
 }
 
 int main()
 {
     nat::init();
-    printf("%-8cexpected iterations / sqrt((p - 1) ln 2)\n", 'k');
-    for (uint32_t k = 1; k < N; ++k)
+    printf("%-8s%-8sexpected iterations / sqrt((p - 1) ln 2)\n", "k_0", "k_1");
+
+    for (uint32_t k1 = 1; k1 < N; ++k1)
     {
-        printf("%-8" PRIu32 "%lf\n", k, expected(k) * log2(2.0 * k));
+        _k[0] = k1;
+        inv_lg_k[0] = 1.0 / log2(2.0 * k1);
+        inv_lg_k[0] *= inv_lg_k[0];
+        for (uint32_t k2 = k1 + 1; k2 < N; ++k2)
+        {
+            _k[1] = k2;
+            inv_lg_k[1] = 1.0 / log2(2.0 * k2);
+            inv_lg_k[1] *= inv_lg_k[1];
+            printf("%-8" PRIu32 "%-8" PRIu32 "%lf\n", k1, k2, expected_f1());
+        }
     }
 }
