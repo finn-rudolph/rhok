fb0c2d31aab48c87a5184b57db66a9c78a5df0ad
Finn Rudolph <finn.rudolph@t-online.de>
2023-12-12 14:58:49 +0100

    Calculate precise average collision length for M=2
    Go over each pair of starting points and take the maximum of their
    number of steps to collision.

--- src/stats.rs
+++ src/stats.rs
@@ -98,184 +98,178 @@ pub fn iota_stats() {
 
 // calculates average collision length (nu) for various k
 pub fn mu_lambda_stats() {
-    // println!(
-    //     "{:<20}{:<16}{:<16}{:<20}{:<20}{:<20}{:<20}{:<20}{:<20}",
-    //     "",
-    //     "k",
-    //     "gcd(k, p - 1)",
-    //     "nu mean",
-    //     "nu std dev",
-    //     "cycle mean",
-    //     "cycle std dev",
-    //     "tail mean",
-    //     "tail std dev"
-    // );
+    println!(
+        "{:<20}{:<16}{:<16}{:<20}{:<20}{:<20}{:<20}{:<20}{:<20}",
+        "",
+        "k",
+        "gcd(k, p - 1)",
+        "nu mean",
+        "nu std dev",
+        "cycle mean",
+        "cycle std dev",
+        "tail mean",
+        "tail std dev"
+    );
 
-    let y: Vec<([f64; K2 + 1], [usize; K2 + 1])> = (A..=B)
-        .into_par_iter()
-        .map(|p| {
-            let mut nu = [0.0; K2 + 1];
-            let mut gcds = [0usize; K2 + 1];
+    (A..=B).into_iter().for_each(|p| {
+        let mut nu = [0.0; K2 + 1];
+        let mut gcds = [0usize; K2 + 1];
 
-            if !miller_rabin(p as u64) {
-                return (nu, gcds);
-            }
+        if !miller_rabin(p as u64) {
+            return;
+        }
 
-            let mtg = Montgomery::new(p as u64);
-            let mut cycle_nodes: Vec<usize> = Vec::new();
-            let mut collision_len: Vec<Vec<usize>> = vec![vec![0; p]; K2 + 1];
-            let mut cycle_len: Vec<usize> = vec![0; p];
-            let mut histogram: Vec<Vec<usize>> = vec![vec![0; p]; K2 + 1];
-            let mut cycle_avg = [0f64; K2 + 1];
+        let mtg = Montgomery::new(p as u64);
+        let mut cycle_nodes: Vec<usize> = Vec::new();
+        let mut collision_len: Vec<Vec<usize>> = vec![vec![0; p]; K2 + 1];
+        let mut cycle_len: Vec<usize> = vec![0; p];
+        let mut histogram: Vec<Vec<usize>> = vec![vec![0; p]; K2 + 1];
+        let mut cycle_avg = [0f64; K2 + 1];
 
-            // println!("{}", p);
+        println!("{}", p);
 
-            for k in K1..=K2 {
-                gcds[k] = gcd(2 * k as u64, p as u64 - 1) as usize;
+        for k in K1..=K2 {
+            gcds[k] = gcd(2 * k as u64, p as u64 - 1) as usize;
 
-                let pre = get_predecessors(p, k, &mtg);
-                let mut visited: Vec<bool> = vec![false; p];
+            let pre = get_predecessors(p, k, &mtg);
+            let mut visited: Vec<bool> = vec![false; p];
 
-                let mut total_collision_len: usize = 0;
-                let mut total_cycle_len: usize = 0;
+            let mut total_collision_len: usize = 0;
+            let mut total_cycle_len: usize = 0;
 
-                for i in 0..p {
-                    if !visited[i] {
-                        {
-                            // mark cycle nodes
-                            let (mut x, mut y) = (i, f(i, k, &mtg));
-                            while x != y {
-                                x = f(x, k, &mtg);
-                                y = f(f(y, k, &mtg), k, &mtg);
-                            }
-
-                            cycle_nodes.clear();
-                            while cycle_nodes.is_empty() || x != cycle_nodes[0]
-                            {
-                                cycle_nodes.push(x);
-                                visited[x] = true;
-                                x = f(x, k, &mtg);
-                            }
+            for i in 0..p {
+                if !visited[i] {
+                    {
+                        // mark cycle nodes
+                        let (mut x, mut y) = (i, f(i, k, &mtg));
+                        while x != y {
+                            x = f(x, k, &mtg);
+                            y = f(f(y, k, &mtg), k, &mtg);
                         }
 
-                        for x in &cycle_nodes {
-                            let mut q: VecDeque<(usize, usize)> =
-                                VecDeque::new();
-                            q.push_back((*x, 0));
+                        cycle_nodes.clear();
+                        while cycle_nodes.is_empty() || x != cycle_nodes[0] {
+                            cycle_nodes.push(x);
+                            visited[x] = true;
+                            x = f(x, k, &mtg);
+                        }
+                    }
 
-                            while !q.is_empty() {
-                                let (u, dis) = *q.front().unwrap();
-                                q.pop_front();
-                                visited[u] = true;
-                                total_collision_len +=
-                                    dis + 2 * cycle_nodes.len();
-                                collision_len[k][u] =
-                                    dis + 2 * cycle_nodes.len();
-                                cycle_len[u] = 2 * cycle_nodes.len();
-                                total_cycle_len += 2 * cycle_nodes.len();
-                                histogram[k][collision_len[k][u]] += 1;
+                    for x in &cycle_nodes {
+                        let mut q: VecDeque<(usize, usize)> = VecDeque::new();
+                        q.push_back((*x, 0));
 
-                                for v in &pre[u] {
-                                    if !visited[*v] {
-                                        q.push_back((*v, dis + 1));
-                                    }
+                        while !q.is_empty() {
+                            let (u, dis) = *q.front().unwrap();
+                            q.pop_front();
+                            visited[u] = true;
+                            total_collision_len += dis + 2 * cycle_nodes.len();
+                            collision_len[k][u] = dis + 2 * cycle_nodes.len();
+                            cycle_len[u] = 2 * cycle_nodes.len();
+                            total_cycle_len += 2 * cycle_nodes.len();
+                            histogram[k][collision_len[k][u]] += 1;
+
+                            for v in &pre[u] {
+                                if !visited[*v] {
+                                    q.push_back((*v, dis + 1));
                                 }
                             }
                         }
                     }
                 }
-
-                let mean_nu = (total_collision_len as f64) / (p as f64);
-                nu[k] = mean_nu;
-                let mean_cycle_len = (total_cycle_len as f64) / (p as f64);
-                let mean_tail_len = ((total_collision_len - total_cycle_len)
-                    as f64)
-                    / (p as f64);
-
-                let mut std_dev_nu = 0.0;
-                let mut std_dev_cycle_len = 0.0;
-                let mut std_dev_tail_len = 0.0;
-                for i in 0..p {
-                    std_dev_nu += (collision_len[k][i] as f64 - mean_nu)
-                        * (collision_len[k][i] as f64 - mean_nu);
-                    std_dev_cycle_len += (cycle_len[i] as f64 - mean_cycle_len)
-                        * (cycle_len[i] as f64 - mean_cycle_len);
-                    std_dev_tail_len += ((collision_len[k][i] - cycle_len[i])
-                        as f64
-                        - mean_tail_len)
-                        * ((collision_len[k][i] - cycle_len[i]) as f64
-                            - mean_tail_len);
-                }
-
-                let g = gcd(2 * k as u64, p as u64 - 1);
-                if g == 2 {
-                    cycle_avg[k] = mean_cycle_len;
-                }
-
-                std_dev_nu = (std_dev_nu / p as f64).sqrt();
-                std_dev_cycle_len = (std_dev_cycle_len / p as f64).sqrt();
-                std_dev_tail_len = (std_dev_tail_len / p as f64).sqrt();
-
-                // println!(
-                //     "{:<20}{:<16}{:<16}{:<20}{:<20}{:<20}{:<20}{:<20}{:<20}",
-                //     "",
-                //     k,
-                //     g,
-                //     mean_nu,
-                //     std_dev_nu,
-                //     mean_cycle_len,
-                //     std_dev_cycle_len,
-                //     mean_tail_len,
-                //     std_dev_tail_len
-                // );
             }
 
-            (nu, gcds)
-        })
-        .collect::<Vec<([f64; K2 + 1], [usize; K2 + 1])>>();
+            let mean_nu = (total_collision_len as f64) / (p as f64);
+            nu[k] = mean_nu;
+            let mean_cycle_len = (total_cycle_len as f64) / (p as f64);
+            let mean_tail_len =
+                ((total_collision_len - total_cycle_len) as f64) / (p as f64);
 
-    for k1 in K1..=K2 {
-        for k2 in k1 + 1..=K2 {
-            let mut len: usize = 0;
-
-            let mut mean1 = 0.0;
-            let mut mean2 = 0.0;
-
-            for (nu, gcds) in &y {
-                if gcds[k1] == 2 && gcds[k2] == 2 {
-                    len += 1;
-                    mean1 += nu[k1];
-                    mean2 += nu[k2];
-                }
+            let mut std_dev_nu = 0.0;
+            let mut std_dev_cycle_len = 0.0;
+            let mut std_dev_tail_len = 0.0;
+            for i in 0..p {
+                std_dev_nu += (collision_len[k][i] as f64 - mean_nu)
+                    * (collision_len[k][i] as f64 - mean_nu);
+                std_dev_cycle_len += (cycle_len[i] as f64 - mean_cycle_len)
+                    * (cycle_len[i] as f64 - mean_cycle_len);
+                std_dev_tail_len += ((collision_len[k][i] - cycle_len[i])
+                    as f64
+                    - mean_tail_len)
+                    * ((collision_len[k][i] - cycle_len[i]) as f64
+                        - mean_tail_len);
             }
 
-            mean1 /= len as f64;
-            mean2 /= len as f64;
-
-            let mut std_dev1 = 0.0;
-            let mut std_dev2 = 0.0;
-
-            for (nu, gcds) in &y {
-                if gcds[k1] == 2 && gcds[k2] == 2 {
-                    std_dev1 += (nu[k1] - mean1) * (nu[k1] - mean1);
-                    std_dev2 += (nu[k2] - mean2) * (nu[k2] - mean2);
-                }
+            let g = gcd(2 * k as u64, p as u64 - 1);
+            if g == 2 {
+                cycle_avg[k] = mean_cycle_len;
             }
 
-            std_dev1 = (std_dev1 / len as f64).sqrt();
-            std_dev2 = (std_dev2 / len as f64).sqrt();
+            std_dev_nu = (std_dev_nu / p as f64).sqrt();
+            std_dev_cycle_len = (std_dev_cycle_len / p as f64).sqrt();
+            std_dev_tail_len = (std_dev_tail_len / p as f64).sqrt();
 
-            let mut corr = 0.0;
-            let prob = 1.0 / len as f64;
-            for (nu, gcds) in &y {
-                if gcds[k1] == 2 && gcds[k2] == 2 {
-                    corr += (nu[k1] - mean1) * (nu[k2] - mean2) * prob;
-                }
-            }
+            println!(
+                "{:<20}{:<16}{:<16}{:<20}{:<20}{:<20}{:<20}{:<20}{:<20}",
+                "",
+                k,
+                g,
+                mean_nu,
+                std_dev_nu,
+                mean_cycle_len,
+                std_dev_cycle_len,
+                mean_tail_len,
+                std_dev_tail_len
+            );
 
-            corr /= std_dev1;
-            corr /= std_dev2;
-            eprintln!("k1 = {:<4} k2 = {:<4} correlation = {}", k1, k2, corr);
+            collision_len[k].sort();
         }
-    }
+
+        for k1 in K1..=K2 {
+            for k2 in k1..=K2 {
+                if gcd(k1 as u64, (p as u64 - 1) / 2) == 1
+                    && gcd(k2 as u64, (p as u64 - 1) / 2) == 1
+                {
+                    let mut sum: usize = 0;
+                    let mut num_samples: usize = 0;
+
+                    for x in &collision_len[k1] {
+                        let (mut a, mut b) = (0, p);
+                        while a < b {
+                            let mid = (a + b) / 2;
+                            if collision_len[k2][mid] <= *x {
+                                a = mid + 1;
+                            } else {
+                                b = mid;
+                            }
+                        }
+                        sum += a * x;
+                        num_samples += a;
+                    }
+
+                    for x in &collision_len[k2] {
+                        let (mut a, mut b) = (0, p);
+                        while a < b {
+                            let mid = (a + b) / 2;
+                            if collision_len[k1][mid] < *x {
+                                a = mid + 1;
+                            } else {
+                                b = mid;
+                            }
+                        }
+                        sum += a * x;
+                        num_samples += a;
+                    }
+
+                    assert_eq!(num_samples, p * p);
+
+                    let expected = (sum as f64) / ((p * p) as f64);
+                    println!(
+                        "k1 = {:<4} k2 = {:<4} expected = {}",
+                        k1, k2, expected
+                    );
+                }
+            }
+        }
+    });
 }
