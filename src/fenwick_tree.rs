0634c229bebc85ccf329bbd7dab3124a958f293a
Finn Rudolph <finn.rudolph@t-online.de>
2023-12-16 23:44:30 +0100

    Clean up the Fenwick Tree by requiring Copy for T

--- src/fenwick_tree.rs
+++ src/fenwick_tree.rs
@@ -4,13 +4,13 @@ pub struct FenwickTree<T>(Vec<T>);
 
 impl<'a, T> FenwickTree<T>
 where
-    T: Default + Clone + 'a + AddAssign<&'a T> + Sub<Output = T>,
+    T: Default + Copy + AddAssign<T> + Sub<Output = T>,
 {
     pub fn new(n: usize) -> FenwickTree<T> {
         FenwickTree(vec![Default::default(); n])
     }
 
-    pub fn update(&mut self, i: usize, delta: &'a T) {
+    pub fn update(&mut self, i: usize, delta: T) {
         let mut j = i as isize + 1;
         while j != self.0.len() as isize {
             self.0[j as usize - 1] += delta;
@@ -18,17 +18,17 @@ where
         }
     }
 
-    pub fn prefix_sum(&'a self, i: usize) -> T {
+    pub fn prefix_sum(&self, i: usize) -> T {
         let mut x: T = Default::default();
         let mut j = i as isize + 1;
         while j != 0 {
-            x += &self.0[j as usize - 1];
+            x += self.0[j as usize - 1];
             j -= j & -j;
         }
         x
     }
 
-    pub fn range_sum(&'a self, i: usize, j: usize) -> T {
+    pub fn range_sum(&self, i: usize, j: usize) -> T {
         if i == 0 {
             self.prefix_sum(j)
         } else {
