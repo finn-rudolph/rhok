ad59b1b4a435417e190c3f54fd7e5b9bc1c6e269
Finn Rudolph <finn.rudolph@t-online.de>
2024-01-13 14:56:15 +0100

    Implement cartesian product iteration

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,49 @@ mod miller_rabin;
 mod montgomery;
 mod pollard_rho;
 
-fn main() {}
+use std::env;
+
+#[derive(Clone, Copy)]
+enum Source {
+    Formula,
+    Real,
+}
+
+fn iterate_k_cartesian_product(
+    machines: usize,
+    k_min: usize,
+    k_max: usize,
+    source: Source,
+    k: &mut Vec<usize>,
+    j: usize,
+) {
+    if j == machines {
+        println!("k = {:?}", k);
+        return;
+    }
+
+    k[j] = if j > 0 { k[j - 1] } else { k_min };
+    while k[j] <= k_max {
+        iterate_k_cartesian_product(machines, k_min, k_max, source, k, j + 1);
+        k[j] += 1;
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert_eq!(args.len(), 5);
+
+    let source = match args[1].as_str() {
+        "--formula" => Source::Formula,
+        "--rela" => Source::Real,
+        _ => unreachable!(),
+    };
+
+    let machines: usize = args[2].parse().unwrap();
+    let k_min: usize = args[3].parse().unwrap();
+    let k_max: usize = args[4].parse().unwrap();
+
+    let mut k = vec![0usize; machines];
+
+    iterate_k_cartesian_product(machines, k_min, k_max, source, &mut k, 0);
+}
