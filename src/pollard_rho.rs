487f93a379942721c4ae9be8e1bc90d35b1012c9
Finn Rudolph <finn.rudolph@t-online.de>
2024-02-22 14:43:20 +0100

    Make rho code actually work again

--- src/pollard_rho.rs
+++ src/pollard_rho.rs
@@ -35,34 +35,34 @@ pub fn pollard_rho(n: u64, k: u64, rng: &mut dyn RngCore) -> u64 {
     const LENGTH_LIMIT: u64 = 1 << 18;
 
     let mtg = Montgomery::new(n);
-    let one = mtg.to_montgomery_space(1);
+    let one = mtg.strict(mtg.to_montgomery_space(1));
     let _k = k << 1;
 
-    let mut x = rng.next_u64() % n;
+    loop {
+        let mut x = rng.next_u64() % n;
 
-    let mut l = BATCH_SIZE;
-    while l <= LENGTH_LIMIT {
-        let (y, mut q) = (x, one);
+        let mut l = BATCH_SIZE;
+        while l <= LENGTH_LIMIT {
+            let (y, mut q) = (x, one);
 
-        let mut i = 0;
-        while i < l {
-            for _ in 0..BATCH_SIZE {
-                x = mtg.add(mtg.pow(x, _k), one);
-                let mut d = x.wrapping_sub(y);
-                d = d.wrapping_add(if (d as i64) < 0 { n } else { 0 });
-                q = mtg.mul(q, d);
+            let mut i = 0;
+            while i < l {
+                for _ in 0..BATCH_SIZE {
+                    x = mtg.strict(mtg.add(mtg.pow(x, _k), one));
+                    let mut d = x.wrapping_sub(y);
+                    d = d.wrapping_add(if (d as i64) < 0 { n } else { 0 });
+                    q = mtg.mul(q, d);
+                }
+
+                let g = gcd(q, n);
+                if g != 1 && g != n {
+                    return g;
+                }
+
+                i += BATCH_SIZE;
             }
 
-            let g = gcd(q, n);
-            if g != 1 && g != n {
-                return g;
-            }
-
-            i += BATCH_SIZE;
+            l <<= 1;
         }
-
-        l <<= 1;
     }
-
-    return u64::MAX;
 }
